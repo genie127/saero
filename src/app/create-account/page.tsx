2231112c036a9061b4b1a290424a024add23a490
genie127 <twt770@naver.com>
2026-05-26 01:29:29 +0900

    빌드진행

--- src/app/create-account/page.tsx
+++ src/app/create-account/page.tsx
@@ -2,7 +2,7 @@
 import { CreateAccount } from "@/features/createAccount/CreateAccount.component";
 import { PageLayout } from "@/shared/components/pageLayout/PageLayout.component";
 
-export default function createAccountPage() {
+export default function CreateAccountPage() {
   return (
     <PageLayout>
       <CreateAccount/>
