d2b907c58e01e6d63041fa3cfb157a0b4ca49776
genie127 <twt770@naver.com>
2026-05-26 01:27:39 +0900

    빌드오류1

--- src/app/create-account/page.tsx
+++ src/app/create-account/page.tsx
@@ -1,7 +1,8 @@
+
 import { CreateAccount } from "@/features/createAccount/CreateAccount.component";
 import { PageLayout } from "@/shared/components/pageLayout/PageLayout.component";
 
-export default function createAccount() {
+export default function createAccountPage() {
   return (
     <PageLayout>
       <CreateAccount/>
